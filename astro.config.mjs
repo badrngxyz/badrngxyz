52c2860d197e6523d4786a3b3ffac38f05d486a1
Unlucky Wanderer <author@badrng.xyz>
2026-04-03 22:52:35 -0300

    2026-04-04T01:52:35Z

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
-// @ts-check
-
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 
 import cloudflare from "@astrojs/cloudflare";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://badrng.xyz",
   integrations: [mdx(), sitemap()],
